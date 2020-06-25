b9b1c675d3f1f2fe268f805f29859eae891612d3
lancerXXXX <2571021108.@qq.com>
2020-06-25 19:00:45 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+  <h1>{{title}}</h1>
+    <form>
+        <el-input placeholder="请输入内容" v-model="loginForm.id" clearable>
+        </el-input>
+        <el-input placeholder="请输入内容" v-model="loginForm.password" show-password>
+        </el-input>
+    </form>
+    <el-button type="primary" plain @click="onSubmit">主要按钮</el-button>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import axios from 'axios';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'app',
+    components: {},
+    data() {
+        return {
+            title: '',
+            loginForm: {
+                "id": '',
+                "password": ''
+            }
+        }
+    },
+    mounted() {
+    },
+    methods: {
+        onSubmit() {
+            let formData = new FormData();
+            for (var key in this.loginForm) {
+                formData.append(key, this.loginForm[key])
+            }
+
+            axios({
+                method: "post",
+                url: "http://localhost:8081",
+                headers: {
+                    "Content-Type": "multipart/form-data"
+                },
+                withCredentials: true,
+                data: formData
+            }).then((res) => {
+                console.log(res);
+            });
+
+        }
+    },
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
+import './plugins/element.js'
 
 Vue.config.productionTip = false
 
